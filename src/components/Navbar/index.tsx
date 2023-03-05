2b8634621e7d6ac84e68c72b29dc0c53a099d291
Farhaan Mukarram <farhaanmukarram@gmail.com>
2023-03-06 02:11:20 +0500

    Fix mobile navbar drawer being visible and not closing on desktop view

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
-import { IconButton } from "@chakra-ui/react";
+import { IconButton, useMediaQuery } from "@chakra-ui/react";
 import { Box, Flex, Heading } from "@chakra-ui/layout";
 
 import NavbarDrawer from "./NavbarDrawer";
@@ -10,6 +10,8 @@ import NavbarLinks from "./NavbarLinks";
 const Navbar = () => {
   // For controlling the state of the mobile version drawer
   const [isNavbarDrawerOpen, setIsNavbarDrawerOpen] = useState(false);
+
+  const [isTablet] = useMediaQuery("(max-width: 767px)");
 
   return (
     <Flex
@@ -32,24 +34,26 @@ const Navbar = () => {
         <NavbarLinks />
       </Box>
 
-      <Box display={{ base: "flex", md: "none" }}>
-        <IconButton
-          aria-label="hamburger menu"
-          icon={
-            isNavbarDrawerOpen ? (
-              <CloseIcon color="blue.500" />
-            ) : (
-              <HamburgerIcon color="blue.500" />
-            )
-          }
-          backgroundColor="white"
-          onClick={() => setIsNavbarDrawerOpen((current) => !current)}
-        />
-        <NavbarDrawer
-          isOpen={isNavbarDrawerOpen}
-          onClose={() => setIsNavbarDrawerOpen(false)}
-        />
-      </Box>
+      {isTablet ? (
+        <Box>
+          <IconButton
+            aria-label="hamburger menu"
+            icon={
+              isNavbarDrawerOpen ? (
+                <CloseIcon color="blue.500" />
+              ) : (
+                <HamburgerIcon color="blue.500" />
+              )
+            }
+            backgroundColor="white"
+            onClick={() => setIsNavbarDrawerOpen((current) => !current)}
+          />
+          <NavbarDrawer
+            isOpen={isNavbarDrawerOpen}
+            onClose={() => setIsNavbarDrawerOpen(false)}
+          />
+        </Box>
+      ) : null}
     </Flex>
   );
 };
